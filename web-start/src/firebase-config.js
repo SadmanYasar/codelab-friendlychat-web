7cb7beeea17ee7adf8d0b971e04ad0e85eb16dcb
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-06 02:56:06 +0600

    added config

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -7,13 +7,18 @@
  * 4. Copy the config object snippet, then add it here.
  */
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyCGXvu0rZK4rWoiCgdNBo6p_PyhWUMNJTs",
+  authDomain: "fir-fireship-learning.firebaseapp.com",
+  projectId: "fir-fireship-learning",
+  storageBucket: "fir-fireship-learning.appspot.com",
+  messagingSenderId: "688918449604",
+  appId: "1:688918449604:web:cc6a99122d6600eb456891"
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+      'Add your web app\'s configuration object to firebase-config.js');
   } else {
     return config;
   }
